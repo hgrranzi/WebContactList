3dd4b82d86321d9b27f4ffd1562048cf02d129eb
hgrranzi <vkh.mariia@gmail.com>
2021-12-02 05:40:44 +0300

    Created servlet drafts

---
+++ src/main/java/ru/hgrranzi/app/servlets/AddContactServlet.java
@@ -0,0 +1,18 @@
+package ru.hgrranzi.app.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddContactServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("dynamic/add.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
